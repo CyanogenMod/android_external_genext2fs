ad9d822cc4b53840b2b6def4986cb4e2be5c30ed
Shashank Mittal <mittals@quicinc.com>
2009-11-16 10:38:27 -0800

    genext2fs: Add support to pass number of blocks as argument
    Change-Id: I6acdc13f61fd5ea83a2a89aaa1b57e96e1170c80

--- Config.mk
+++ Config.mk
@@ -2,6 +2,7 @@
 # $(2): output file
 # $(3): label (if any)
 # $(4): if true, add journal
+# $(5): num_block
 define build-userimage-ext2-target
 	@mkdir -p $(dir $(2))
 	$(hide) num_blocks=`du -sk $(1) | tail -n1 | awk '{print $$1;}'`;\
@@ -9,6 +10,7 @@ define build-userimage-ext2-target
 	else extra_blocks=20480; fi ; \
 	num_blocks=`expr $$num_blocks + $$extra_blocks` ; \
 	num_inodes=`find $(1) | wc -l` ; num_inodes=`expr $$num_inodes + 500`; \
+	if [ $(5) ]; then num_blocks=$(5); fi; \
 	$(MKEXT2IMG) -a -d $(1) -b $$num_blocks -N $$num_inodes -m 0 $(2)
 	$(if $(strip $(3)),\
 		$(hide) $(TUNE2FS) -L $(strip $(3)) $(2))
