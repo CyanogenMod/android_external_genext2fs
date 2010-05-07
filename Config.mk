4c6c6f56ec399911c3722c4eb06b346905a57b02
Colin Cross <ccross@android.com>
2010-05-07 15:27:39 -0700

    genext2fs: Remove extra $(hide)
    Change-Id: Ic330aeb21a674e1aebcb778e5281610228a22a53

--- Config.mk
+++ Config.mk
@@ -11,7 +11,7 @@ define build-userimage-ext-target
 	else extra_blocks=20480; fi ; \
 	num_blocks=`expr $$num_blocks + $$extra_blocks` ; \
 	num_inodes=`find $(1) | wc -l` ; num_inodes=`expr $$num_inodes + 500`; \
-	$(hide) $(MKEXT2IMG) -a -d $(1) -b $$num_blocks -N $$num_inodes -m 0 $(2)
+	$(MKEXT2IMG) -a -d $(1) -b $$num_blocks -N $$num_inodes -m 0 $(2)
 	$(if $(strip $(3)),\
 		$(hide) $(TUNE2FS) -L $(strip $(3)) $(2))
 	$(if $(filter ext3,$(4)), \
