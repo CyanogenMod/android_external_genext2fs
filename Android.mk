2e672fab1b8935ab6df5c2fc69386fe6b4ade873
Colin Cross <ccross@android.com>
2010-08-03 14:49:20 -0700

    Move mkuserimg.sh to make_ext4fs
    Change-Id: Ic2aa46287e961622702ed1c0c41246f4c2ca9067

--- Android.mk
+++ Android.mk
@@ -13,18 +13,3 @@ LOCAL_CFLAGS := -O2 -g -Wall -DHAVE_CONFIG_H
 LOCAL_MODULE := genext2fs
 
 include $(BUILD_HOST_EXECUTABLE)
-
-################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := mkuserimg.sh
-LOCAL_SRC_FILES := mkuserimg.sh
-LOCAL_MODULE_CLASS := EXECUTABLES
-# We don't need any additional suffix.
-LOCAL_MODULE_SUFFIX :=
-LOCAL_BUILT_MODULE_STEM := $(notdir $(LOCAL_SRC_FILES))
-LOCAL_IS_HOST_MODULE := true
-
-include $(BUILD_PREBUILT)
-
-################################
